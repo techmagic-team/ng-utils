881ad8019543999e11b888a3e3d02cace90909c3
graniron <roman.yavoriv@techmagic.co>
2018-08-22 11:13:42 +0300

    Clean initial CLI setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngu-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-utils';
-}
+export class AppComponent {}
