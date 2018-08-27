da754db22c1fd83bfbab8e80caa12284deb3acf1
graniron <roman.yavoriv@techmagic.co>
2018-08-27 11:47:03 +0300

    Tests for throttleClickDirective

--- projects/ng-utils-lib/src/lib/directives/throttle-click.directive.spec.ts
+++ projects/ng-utils-lib/src/lib/directives/throttle-click.directive.spec.ts
@@ -1,8 +1,66 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { ThrottleClickDirective } from './throttle-click.directive';
 
+const testThrottleTime = 500;
+
+// Test wrapper component
+@Component({
+  template: `
+    <button
+      (nguThrottleClick)="onClick()"
+      [throttleTime]="testThrottleTime"
+    >Click Test</button>
+  `,
+})
+class TestThrottleClickComponent {
+  onClick() {}
+}
+
 describe('ThrottleClickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ThrottleClickDirective();
-    expect(directive).toBeTruthy();
+  let component: TestThrottleClickComponent;
+  let fixture: ComponentFixture<TestThrottleClickComponent>;
+  let btnEl: DebugElement;
+  const event = {
+    preventDefault() {},
+    stopPropagation() {},
+  };
+
+  beforeEach(() => {
+    jasmine.clock().uninstall();
+    jasmine.clock().install();
+
+    fixture = TestBed.configureTestingModule({
+      declarations: [ TestThrottleClickComponent, ThrottleClickDirective ],
+    })
+    .createComponent(TestThrottleClickComponent);
+
+    fixture.detectChanges(); // initial binding
+
+    component = fixture.componentInstance;
+    btnEl = fixture.debugElement.query(By.css('button'));
+  });
+
+  it('should call onClick()', () => {
+    spyOn(component, 'onClick');
+
+    btnEl.triggerEventHandler('click', event);
+
+    expect(component.onClick).toHaveBeenCalled();
+  });
+
+  it(`should call onClick() not often then declared throttle time`, () => {
+    spyOn(component, 'onClick');
+
+    btnEl.triggerEventHandler('click', event);
+    btnEl.triggerEventHandler('click', event); // event will be ignored
+    btnEl.triggerEventHandler('click', event); // event will be ignored
+
+    jasmine.clock().tick(testThrottleTime);
+    btnEl.triggerEventHandler('click', event);
+
+    expect(component.onClick).toHaveBeenCalledTimes(2);
   });
 });
